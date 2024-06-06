059e10c923aafc15cf88a56fbb6b94a584b23341
kbkuba <jakubgrzywa@gmail.com>
2024-06-06 23:07:29 +0200

    Initial comit

---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-220, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(arg=f"Level: {self.level}", move=False, align="center", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg=f"GAME OVER", move=False, align="center", font=FONT)
